a36a9ad006d7a8631a75e333cf2528ca68b37816
Paulo Junior <ruck18@gmail.com>
2023-01-19 21:03:36 -0300

    3

--- gestorssh/app-assets/images/index.php
+++
@@ -1,5 +0,0 @@
-        <iframe width="100%" height="100%" src="index.html" 
-allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                             
-
-
-
