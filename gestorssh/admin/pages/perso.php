f6d10772f6e01b62292fffee4ed71ae4225dbb4f
Paulo Junior <ruck18@gmail.com>
2023-01-23 22:28:21 -0300

    UP
    update

--- gestorssh/admin/pages/perso.php
+++ gestorssh/admin/pages/perso.php
@@ -1,3 +1,22 @@
+<style type="text/css">
+.perso {
+	background-color: #639;
+	border: solid 1px #FF3333;
+	border-radius: 10px;
+	height: 30px;
+	color: #FFF;
+}
+.btapp {
+	color: #333;
+	background-color: #6C6;
+	border: solid 1px #FF3333;
+	border-radius: 10px;
+	height: 30px;
+}
+#multiple-column-form .row .col-12 .card .card-header .card-title {
+	color: #F00;
+}
+</style>
 <section id="multiple-column-form">
     <div class="row">
         <div class="col-12">
@@ -8,28 +27,28 @@
 					
                 </div>
                 <div class="card-body">
-<center><p class="px-0"><b>AQUI FAZ UPLOAD DO BACKGROUND REVENDA.</b></p></center>
+<center><p class="px-0"><b>AQUI FAZ UPLOAD DO LOGO PAINEL.</b></p></center>
 <div class="alert-info">						
 </div>
 <p> 
 </p>     
 <form method="post" enctype="multipart/form-data" action="recebeBkrv.php">
        
-	  <center><input name="arquivo" type="file" />
-       <input type="submit" value="Salvar" /> </center><br/>
+	  <center><input name="arquivo" type="file" class="perso" />
+       <input type="submit" class="btapp" value="Salvar" /> </center><br/>
 	   <p>
 				</p>
     </form>
 </div>
-<center><p class="px-0"><b>AQUI FAZ UPLOAD  DO BACKGROUND ADMIN.</b></p></center>
+<center><p class="px-0"><b>AQUI FAZ UPLOAD DA LOGO ADMIN.</b></p></center>
 <div class="alert-info">						
 </div>
 <p> 
 </p>     
 <form method="post" enctype="multipart/form-data" action="recebeBkad.php">
        
-	  <center><input name="arquivo" type="file" />
-       <input type="submit" value="Salvar" /> </center><br/>
+	  <center><input name="arquivo" type="file" class="perso" />
+       <input type="submit" class="btapp" value="Salvar" /> </center><br/>
 	   <p>
 				</p>		
     </form>
@@ -41,8 +60,8 @@
 </p>     
 <form method="post" enctype="multipart/form-data" action="recebeStore.php">
        
-	  <center><input name="arquivo" type="file" />
-       <input type="submit" value="Salvar" /> </center><br/>
+	  <center><input name="arquivo" type="file" class="perso" />
+       <input type="submit" class="btapp" value="Salvar" /> </center><br/>
     </form>	
                 </div>
             </div>
